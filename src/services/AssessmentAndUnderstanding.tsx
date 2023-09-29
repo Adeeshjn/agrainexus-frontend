d5f0ab1fe380fa437b3799c70b1644e8d9c479e3
Chaitanya-NK <chaitanya.nk2002@gmail.com>
2023-09-29 18:10:34 +0530

    changes in assessment and understanding

--- src/services/AssessmentAndUnderstanding.tsx
+++ src/services/AssessmentAndUnderstanding.tsx
@@ -1,12 +1,20 @@
-import React from "react";
-import { TextField, Button, Paper, Grid, Typography } from "@mui/material";
+import React, { useState } from "react";
+import { TextField, Button, Paper, Grid, Typography, Select, MenuItem, SelectChangeEvent } from "@mui/material";
 import AddLocationIcon from '@mui/icons-material/AddLocation';
 import GrassIcon from '@mui/icons-material/Grass';
 import HeightIcon from '@mui/icons-material/Height';
 
+// Define an interface for the state variables
+interface FarmData {
+    location: string;
+    crops: string[];
+    areaValue: number;
+    areaUnit: string;
+}
+
 const formContainerStyle = {
-    width: "300px", // Adjust the width as per your preference
-    margin: "20px auto", // Add margin-top to push it down
+    width: "300px",
+    margin: "20px auto",
     padding: "20px",
 };
 
@@ -14,7 +22,48 @@ const buttonStyle = {
     marginTop: "20px",
 };
 
+const areaUnits = ["acres", "hectares", "square meters", "square feet", "square kilometers"];
+
 export default function AssessmentAndUnderstanding() {
+    // Use the FarmData interface for the state variable
+    const [farmData, setFarmData] = useState<FarmData>({
+        location: "",
+        crops: [],
+        areaValue: 0,
+        areaUnit: "acres", // Default unit, you can set to an appropriate default
+    });
+
+    const handleAddFarm = () => {
+        // Concatenate areaValue and areaUnit into a single string
+        const farmLocation = `${farmData.location}`;
+        const areaInfo = `${farmData.areaValue} ${farmData.areaUnit}`;
+
+        console.log("Adding Farm:", {
+            location: farmLocation,
+            crops: farmData.crops, // No need to split, it's already an array
+            areaValue: areaInfo, // Set the area info as a single string
+        });
+
+        setFarmData({
+            location: "",
+            crops: [],
+            areaValue: 0,
+            areaUnit: "acres", // Reset to default unit after adding
+        });
+    };
+
+    const handleAreaValueChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        // Ensure the input is a valid number
+        const numericValue = parseFloat(e.target.value);
+        if (!isNaN(numericValue)) {
+            setFarmData({ ...farmData, areaValue: numericValue });
+        }
+    };
+
+    const handleAreaUnitChange = (e: SelectChangeEvent<string>) => {
+        setFarmData({ ...farmData, areaUnit: e.target.value as string });
+    };
+
     return (
         <Paper elevation={3} style={formContainerStyle}>
             <Typography variant="h5" align="center" style={{ marginBottom: '5px' }}>
@@ -29,9 +78,11 @@ export default function AssessmentAndUnderstanding() {
                             label="Location of Farm"
                             placeholder="Location of Farm"
                             variant="outlined"
+                            value={farmData.location}
+                            onChange={(e) => setFarmData({ ...farmData, location: e.target.value })}
                             InputProps={{
                                 style: {
-                                    fontSize: '16px', // Set your desired font size here
+                                    fontSize: '16px',
                                 },
                                 endAdornment: (
                                     <AddLocationIcon />
@@ -47,9 +98,11 @@ export default function AssessmentAndUnderstanding() {
                             placeholder="Enter 1 crop per line"
                             multiline
                             variant="outlined"
+                            value={farmData.crops.join('\n')}
+                            onChange={(e) => setFarmData({ ...farmData, crops: e.target.value.split('\n') })}
                             InputProps={{
                                 style: {
-                                    fontSize: '16px', // Set your desired font size here
+                                    fontSize: '16px',
                                 },
                                 endAdornment: (
                                     <GrassIcon />
@@ -57,22 +110,40 @@ export default function AssessmentAndUnderstanding() {
                             }}
                         />
                     </Grid>
-                    <Grid item xs={12}>
+                    <Grid item xs={6}>
                         <TextField
                             fullWidth
-                            id="area-of-farm"
-                            label="Area of the Farm"
-                            placeholder="Area of the Farm"
+                            id="area-value"
+                            label="Area Value"
+                            placeholder="Area Value"
                             variant="outlined"
+                            type="number"
+                            value={farmData.areaValue}
+                            onChange={handleAreaValueChange}
                             InputProps={{
                                 style: {
-                                    fontSize: '16px', // Set your desired font size here
+                                    fontSize: '16px',
                                 },
-                                endAdornment: (
-                                    <HeightIcon style={{ transform: 'rotate(90deg)' }} />
-                                ),
                             }}
                         />
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Select
+                            fullWidth
+                            id="area-unit"
+                            label="Area Unit"
+                            placeholder="Area Units"
+                            variant="outlined"
+                            value={farmData.areaUnit}
+                            onChange={handleAreaUnitChange}
+                            style={{ fontSize: '16px' }}
+                        >
+                            {areaUnits.map((unit) => (
+                                <MenuItem key={unit} value={unit}>
+                                    {unit}
+                                </MenuItem>
+                            ))}
+                        </Select>
                     </Grid>
                     <Grid item xs={12}>
                         <Button
@@ -80,9 +151,7 @@ export default function AssessmentAndUnderstanding() {
                             color="primary"
                             fullWidth
                             style={buttonStyle}
-                            onClick={() => {
-                                // Handle the "Add Farm" button click here
-                            }}
+                            onClick={handleAddFarm}
                         >
                             Add Farm
                         </Button>
